0aa9b706e44e7c6ad0f8c45a2a814f9163a6a950
FLiotta <32228156+FLiotta@users.noreply.github.com>
2023-08-05 06:33:59 -0300

    Updated title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'PuppySignal',
+  title: 'Documentation',
   tagline: 'Pet QRCode Tag',
   favicon: 'img/favicon.ico',
 
